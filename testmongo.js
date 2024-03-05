c29c26e6d31d77cfed15cd568d3cac8507b099d1
PaytonLaskie <113700170+PaytonLaskie@users.noreply.github.com>
2024-03-04 19:11:48 -0600

    Update testmongo.js

--- testmongo.js
+++ testmongo.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://paytonlaskie:<ILoveMyCatSimba2020!>@cluster0.pkyg2gg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
